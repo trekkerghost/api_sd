d53767774809408a60be0510604b6751115274e9
Daniel Segovia <segoviadaniel+git@gmail.com>
2018-02-19 02:33:28 -0500

    implementacion de POST

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.views import View
 from django.db.models import ObjectDoesNotExist
 
-from api.models import Product
+from api.models import Product, ProductTags
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ class APIProductos(View):
     def get(self, *args, **kwargs):
         '''Definición de método GET para listar y obtener'''
         if 'pk' in kwargs:
-        #Si existe un parámetro en la URL estamos buscando un producto
+            #Si existe un parámetro en la URL estamos buscando un producto
             try:
                 id_producto = kwargs.get('id', None)
                 producto = Product.objects.prefetch_related('tags').get(
@@ -50,3 +50,33 @@ class APIProductos(View):
                 return JsonResponse(lista_productos, safe=False)
             else:
                 return JsonResponse({'response': 'No existen productos'})
+
+    def post(self, request, *args, **kwargs):
+        post_data = request.POST.copy()
+
+        if post_data:
+            tags = [ProductTags(name=tag) for tag in post_data.get('tags')]
+            producto = Product(
+                name=post_data.get('name'),
+                description=post_data.get('description'),
+                quantity=post_data.get('quantity'))
+            try:
+                producto.save()
+                producto.tags.add(tags)
+                producto.save()
+                return JsonResponse(
+                    {
+                        'response': 'Producto creado satisfactoriamente',
+                        'id': producto.pk
+                    },
+                    safe=False)
+            except Exception:
+                return JsonResponse(
+                    {
+                        'response': 'Error al crear el producto'
+                    }, safe=False)
+        else:
+            return JsonResponse(
+                {
+                    'response': 'Petición incorrecta'
+                }, safe=False)
