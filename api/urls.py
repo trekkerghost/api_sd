00e54a23b8a85311e40896c11b7b57a4d4630c52
Daniel Segovia <segoviadaniel+git@gmail.com>
2018-02-19 00:57:14 -0500

    urls de la API

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+'''Definicion de vistas de la API'''
+from django.conf.urls import url
+
+from api.views import APIProductos
+
+urlpatterns = [
+    url(r'^list/', APIProductos.as_view(), name='list'),
+    url(r'^create/', APIProductos.as_view(), name='create'),
+    url(r'^get/(?P<pk>[0-9]+)/$', APIProductos.as_view(), name='get'),
+    url(r'^update/(?P<pk>[0-9]+)/$', APIProductos.as_view(), name='update'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', APIProductos.as_view(), name='delete'),
+]
