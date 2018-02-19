875529b095c5de73d4f800c6630d1516e8ecad8b
Daniel Segovia <segoviadaniel+git@gmail.com>
2018-02-19 01:29:31 -0500

    implementacion de GET

--- api/views.py
+++ api/views.py
@@ -1,3 +1,51 @@
+'''Definicion de vistas de la API'''
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+from django.db.models import ObjectDoesNotExist
+
+from api.models import Product
 
 # Create your views here.
+
+
+class APIProductos(View):
+    def get(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            try:
+                id_producto = kwargs.get('id', None)
+                producto = Product.objects.prefetch_related('tags').get(
+                    pk=id_producto)
+                return JsonResponse(
+                    {
+                        'id':
+                        producto.pk,
+                        'name':
+                        producto.description,
+                        'quantity':
+                        producto.quantity,
+                        'tags': [{
+                            'id': tag.id,
+                            'name': tag.name
+                        } for tag in producto.tag.all()]
+                    },
+                    safe=False)
+            except ObjectDoesNotExist:
+                return JsonResponse({'response': 'Producto no encontrado'})
+        else:
+            lista_productos = [{
+                'id':
+                producto.pk,
+                'name':
+                producto.description,
+                'quantity':
+                producto.quantity,
+                'tags': [{
+                    'id': tag.id,
+                    'name': tag.name
+                } for tag in producto.tag.all()]
+            } for producto in Product.objects.prefetch_related('tags').all()]
+            if lista_productos:
+                return JsonResponse(lista_productos, safe=False)
+            else:
+                return JsonResponse({'response': 'No existen productos'})
